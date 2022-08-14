ce5ecf991a103f5a07e18a330e1bac01e4c9fd7e
Rafi Fuady <rafifuady@gmail.com>
2022-08-14 15:05:14 +0700

    feat: init container

---
+++ src/modules/pokedex/containers/PokedexListContainer.jsx
@@ -0,0 +1,43 @@
+import { Paper } from '@mui/material';
+import React from 'react';
+
+function PokedexListContainer() {
+  return (
+    <div >
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+      <Paper sx={{ margin: '2em' }}>aa</Paper>
+    </div>
+  );
+}
+
+export default PokedexListContainer;
--- src/pages/pokedex/PokedexListPage.jsx
+++ src/pages/pokedex/PokedexListPage.jsx
@@ -1,7 +1,16 @@
+import { Box, Container, Stack } from '@mui/material';
 import React from 'react';
+import { PokedexListContainer } from '../../modules/pokedex/containers';
 
 function PokedexListPage() {
-  return <div>PokedexListPage</div>;
+  return (
+    <Box>
+      <Stack direction='row'  justifyContent='space-around' sx={{ height: '100vh' }}>
+        <Container sx={{ background: 'red' }}>image</Container>
+        <Container sx={{ background: 'gray', overflowY: 'scroll' }}><PokedexListContainer /></Container>
+      </Stack>
+    </Box>
+  );
 }
 
 export default PokedexListPage;
