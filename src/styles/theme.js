c8e988ad07967561a3a236518f16c8f99cf040ea
Rafi Fuady <rafifuady@gmail.com>
2022-08-14 10:07:54 +0700

    feat: init MUI minimal template

---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material/styles';
+
+// Create a theme instance.
+const theme = createTheme({
+  // palette: {
+  //   primary: {
+  //     main: '#2196f3',
+  //   },
+  //   secondary: {
+  //     main: '#f37f21',
+  //   },
+  //   background: {
+  //     default: '#eeeeee',
+  //   },
+  // },
+});
+
+export default theme;
