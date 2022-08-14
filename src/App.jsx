a50a06616ff9bb1eff0d5cf665da1f79162375f2
Rafi Fuady <rafifuady@gmail.com>
2022-08-14 10:28:13 +0700

    feat[theme]: init MUI8 on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-function App() {
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * MUI Imports
+ */
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+
+/**
+ * custom file Imports
+ */
+import theme from './styles/theme';
+import createEmotionCache from './common/utils/createEmotionCache';
+
+const clientSideEmotionCache = createEmotionCache();
+
+function App({ emotionCache = clientSideEmotionCache }) {
   return (
-    <>
-      POKEDEX
-    </>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        POKEDEX
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
+
+App.propTypes = {
+  emotionCache: PropTypes.element.isRequired,
+};
 
 export default App;
