451a3b0d9cd1ef724d562c9a48ab30f65b1b0b09
Rafi Fuady <rafifuady@gmail.com>
2022-08-15 08:55:56 +0700

    feat[pokedex]: api integration

--- src/modules/pokedex/containers/PokedexListContainer.jsx
+++ src/modules/pokedex/containers/PokedexListContainer.jsx
@@ -1,41 +1,48 @@
 import { Paper } from '@mui/material';
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { pokedexActions } from '../_redux/pokedex.actions';
 
 function PokedexListContainer() {
+  const pokedex = useSelector((state) => state.pokedex);
+  const dispatch = useDispatch();
+  const observer = useRef();
+
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const lastBookElementRef = useCallback(
+    (node) => {
+      if (pokedex.isLoading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && pokedex.hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [pokedex.isLoading, pokedex.hasMore]
+  );
+
+  useEffect(() => {
+    dispatch(
+      pokedexActions.getList(
+        {
+          limit: 20,
+          offset: pageNumber * 20,
+        },
+        pokedex.results
+      )
+    );
+  }, [pageNumber]);
+
   return (
-    <div >
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
-      <Paper sx={{ margin: '2em' }}>aa</Paper>
+    <div>
+      {pokedex.results.map((val, id) => (
+        <Paper key={id} sx={{ margin: '2em' }} ref={lastBookElementRef}>
+          {val.name}
+        </Paper>
+      ))}
     </div>
   );
 }
